d20ceb9cb88ddd9eafd39cb0c3b59e22db9cb405
Levi Hayes <levi.hayes@oit.edu>
2018-11-07 19:09:30 -0800

    Add project files.

---
+++ Levi Hayes Lab 6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Levi_Hayes_Lab_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
